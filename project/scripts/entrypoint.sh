f02505aea41896d3af172c8e511e977aee12b1cd
SaenGate <saengate@gmail.com>
2020-06-13 15:59:13 -0400

    fix travis test and entrypoint.sh

--- project/scripts/entrypoint.sh
+++ project/scripts/entrypoint.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
 service ssh start
+
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts
 cd ~/ansible
 ansible-playbook config-project.yml
+
+# Resuelve el problema "supervisor.sock permiso denegado"
+echo_supervisord_conf > /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf
+
+supervisorctl reread
+supervisorctl update
+supervisorctl start all
+
+service supervisor start
+service nginx start
+
+cd /var/www/project
 /bin/bash
